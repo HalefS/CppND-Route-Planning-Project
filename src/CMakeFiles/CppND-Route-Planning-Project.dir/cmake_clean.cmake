6b86efb4112895ed19c9ab34cf41e39ccead95c4
Halef Spencer <halef.spencer@gmail.com>
2020-01-06 11:28:53 -0800

    Add get user coordinates

---
+++ src/CMakeFiles/CppND-Route-Planning-Project.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "../bin/CppND-Route-Planning-Project"
+  "../bin/CppND-Route-Planning-Project.pdb"
+  "CMakeFiles/CppND-Route-Planning-Project.dir/main.cpp.o"
+  "CMakeFiles/CppND-Route-Planning-Project.dir/model.cpp.o"
+  "CMakeFiles/CppND-Route-Planning-Project.dir/render.cpp.o"
+  "CMakeFiles/CppND-Route-Planning-Project.dir/route_model.cpp.o"
+  "CMakeFiles/CppND-Route-Planning-Project.dir/route_planner.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppND-Route-Planning-Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,17 @@
 
 using namespace std::experimental;
 
+bool getCoordinates(float &start_x, float &start_y, float &end_x, float &end_y);
+
 static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
-{   
+{
     std::ifstream is{path, std::ios::binary | std::ios::ate};
     if( !is )
         return std::nullopt;
-    
+
     auto size = is.tellg();
-    std::vector<std::byte> contents(size);    
-    
+    std::vector<std::byte> contents(size);
+
     is.seekg(0);
     is.read((char*)contents.data(), size);
 
@@ -28,7 +30,7 @@ static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
 }
 
 int main(int argc, const char **argv)
-{    
+{
     std::string osm_data_file = "";
     if( argc > 1 ) {
         for( int i = 1; i < argc; ++i )
@@ -36,11 +38,11 @@ int main(int argc, const char **argv)
                 osm_data_file = argv[i];
     }
     else {
-        std::cout << "Usage: [executable] [-f filename.osm]" << std::endl;    
+        std::cout << "Usage: [executable] [-f filename.osm]" << std::endl;
     }
-    
+
     std::vector<std::byte> osm_data;
- 
+
     if( osm_data.empty() && !osm_data_file.empty() ) {
         std::cout << "Reading OpenStreetMap data from the following file: " <<  osm_data_file << std::endl;
         auto data = ReadFile(osm_data_file);
@@ -49,16 +51,21 @@ int main(int argc, const char **argv)
         else
             osm_data = std::move(*data);
     }
-    
+
     // TODO: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below.
+    float start_x, start_y, end_x, end_y;
+    bool validInput = false;
+    while (!validInput) {
+      validInput = getCoordinates(start_x, start_y, end_x, end_y);
+    }
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Perform search and render results.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_y, end_y};
     Render render{model};
 
     auto display = io2d::output_surface{400, 400, io2d::format::argb32, io2d::scaling::none, io2d::refresh_style::fixed, 30};
@@ -69,4 +76,28 @@ int main(int argc, const char **argv)
         render.Display(surface);
     });
     display.begin_show();
+}
+
+bool getCoordinates(float &start_x, float &start_y, float &end_x, float &end_y) {
+  std::vector<float> coordinates{};
+  std::cout << "Please enter start_x :" << std::endl;
+  std::cin >> start_x;
+  std::cout << "Please enter start_y :" << std::endl;
+  std::cin >> start_y;
+  std::cout << "Please enter end_x :" << std::endl;
+  std::cin >> end_x;
+  std::cout << "Please enter end_y :" << std::endl;
+  std::cin >> end_y;
+  coordinates.push_back(start_x);
+  coordinates.push_back(start_y);
+  coordinates.push_back(end_x);
+  coordinates.push_back(end_y);
+  for(float &value : coordinates) {
+    if(value < 0 || value > 100) {
+      std::cout << "Invalid coordinates, please enter values > 0 and < 100" << std::endl;
+      return false;
+    }
+  }
+
+  return true;
 }
